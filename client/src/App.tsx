e6db5057aa577d3138c836c88b804c73f2d2fc2c
puneeth rao <puneethrao@puneeths-MacBook-Air.local>
2026-01-05 22:01:09 +0000

    Stable working state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,28 @@
 import { Switch, Route } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "./pages/home";
-import SalesforceDemo from "./pages/salesforce-demo";
+import Home from "@/pages/home";
+import SalesforceDemo from "@/pages/salesforce-demo";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/salesforce-demo" component={SalesforceDemo} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <div hook={useHashLocation}>
-          <Switch>
-            <Route path="/" component={Home} />
-            <Route path="/salesforce-demo" component={SalesforceDemo} />
-          </Switch>
-        </div>
         <Toaster />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
