5d3ae02545ffa23a7cf7c2ac4ca39420ac6328c9
puneeth rao <puneethrao@puneeths-MacBook-Air.local>
2026-01-05 20:53:36 +0000

    Add hash location to wouter for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,11 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
+import { useHashLocation } from "wouter/use-hash-location";import Home from "@/pages/home";
 import SalesforceDemo from "@/pages/salesforce-demo";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+<Switch location={useHashLocation()}>
   return (
     <Switch>
       <Route path="/" component={Home} />
