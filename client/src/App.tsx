1ee7edb883edd8c690e83127d748c1826af5a4b5
puneeth rao <puneethrao@puneeths-MacBook-Air.local>
2026-01-05 21:49:46 +0000

    Fix hash router imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,23 @@
 import { Switch, Route } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
-import SalesforceDemo from "@/pages/salesforce-demo";
-import NotFound from "@/pages/not-found";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/salesforce-demo" component={SalesforceDemo} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import Home from "./pages/home";
+import SalesforceDemo from "./pages/salesforce-demo";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <div hook={useHashLocation}>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/salesforce-demo" component={SalesforceDemo} />
+          </Switch>
+        </div>
         <Toaster />
-        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
