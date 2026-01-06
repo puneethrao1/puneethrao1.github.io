9a8e301522ea45788661edaa28d2973e8978ecd9
puneeth rao <puneethrao@puneeths-MacBook-Air.local>
2026-01-06 15:51:09 +0000

    Fix Router - use wouter Router component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,25 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
-import SalesforceDemo from "@/pages/salesforce-demo";
-import NotFound from "@/pages/not-found";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/salesforce-demo" component={SalesforceDemo} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import Home from "./pages/home";
+import SalesforceDemo from "./pages/salesforce-demo";
+import NotFound from "./pages/not-found";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <Router hook={useHashLocation}>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/salesforce-demo" component={SalesforceDemo} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
         <Toaster />
-        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
